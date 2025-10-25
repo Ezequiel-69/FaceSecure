1bf995740a569cbc3747c20ceb185c498d465bde
Yordle <cri.collao@duocuc.cl>
2025-10-25 20:21:39 -0300

    Agregadas las pantallas con botones y sonidos para cada boton

--- app/src/main/java/com/example/facesecure/MainActivity.kt
+++ app/src/main/java/com/example/facesecure/MainActivity.kt
@@ -1,48 +1,57 @@
-
 package com.example.facesecure
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.facesecure.ui.screens.FacialRecognitionScreen
+import com.example.facesecure.ui.screens.HomeScreen
+import com.example.facesecure.ui.screens.LoginScreen
+import com.example.facesecure.ui.screens.RegisterScreen
+import com.example.facesecure.ui.screens.StartScreen
 import com.example.facesecure.ui.theme.FaceSecureTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FaceSecureTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "start") {
+        composable("start") {
+            StartScreen(navController = navController)
+        }
+        composable("login") {
+            LoginScreen(navController = navController)
+        }
+        composable("register") {
+            RegisterScreen(navController = navController)
+        }
+        composable("facial_recognition") {
+            FacialRecognitionScreen(navController = navController)
+        }
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     FaceSecureTheme {
-        Greeting("Android")
+        AppNavigation()
     }
 }
